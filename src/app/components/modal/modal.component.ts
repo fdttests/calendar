908117a2e4bd67063e34de57e1780aa9954c052c
Fdttests <fdttests@gmail.com>
2022-04-28 21:16:53 -0300

    refactor: change modal style & animation

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -7,9 +7,13 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
   styleUrls: ['./modal.component.css'],
   animations: [
     trigger('fade', [
-      state('void', style({ opacity: 0 })),
-      transition(':enter, :leave', [
-        animate(300)
+      state('in', style({ opacity:1,transform: 'translateY(0)' })),
+      transition('void => *', [
+        style({ opacity:0,transform: 'translateY(100%)' }),
+        animate(200)
+      ]),
+      transition('* => void', [
+        animate(200, style({ opacity:0,transform: 'translateY(100%)' }))
       ])
     ])
   ]
