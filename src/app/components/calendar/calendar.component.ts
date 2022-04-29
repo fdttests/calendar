e7d1ae7f59bb2e651c5e8e77a972beb0aa7db6c8
Fdttests <fdttests@gmail.com>
2022-04-28 21:16:51 -0300

    wip: render calendar days logic

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { GetDateRangeUseCase } from 'src/app/data/use-cases/date/get-date-range.use-case';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-export class CalendarComponent implements OnInit {
+export class CalendarComponent implements OnChanges, OnInit {
 
-  constructor() { }
+  @Input() public selectedDate: Date = new Date();
 
-  ngOnInit(): void {
+  public dateRange: Array<Date> = [];
+
+  public constructor(
+    private getDateRangeUseCase: GetDateRangeUseCase
+  ) { }
+
+  public ngOnInit(): void {
+    this.applyDateInterval(this.selectedDate);
   }
 
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes['selectedDate'].currentValue) {
+      this.applyDateInterval(changes['selectedDate'].currentValue);
+    }
+  }
+
+  private applyDateInterval(date: Date) {
+    this.dateRange = this.getDateRangeUseCase.execute(date);
+  }
 }
