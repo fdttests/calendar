23078cb0c28c4a41572f4599414bb657a7f9354b
Fdttests <fdttests@gmail.com>
2022-04-28 21:16:55 -0300

    feat: generate id & update method in reminder store

--- src/app/data/stores/reminder/reminder.store.ts
+++ src/app/data/stores/reminder/reminder.store.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { createStore, select, setProps, Store, withProps } from "@ngneat/elf";
-import { addEntities, selectAllEntitiesApply, withEntities } from "@ngneat/elf-entities";
+import { addEntities, selectAllEntitiesApply, updateEntities, withEntities } from "@ngneat/elf-entities";
 import { switchMap } from "rxjs";
 import ReminderFilterModel from "../../models/reminder/reminder-filter.model";
 import ReminderModel from "../../models/reminder/reminder.model";
@@ -23,9 +23,17 @@ export default class ReminderStore {
     }
 
     public add(reminder: ReminderModel) {
+        reminder.id = Date.now().toString(36) + Math.random().toString(36).substring(2);
+
         this.store.update(
             addEntities(reminder)
         );
+    }
+
+    public update(reminder: ReminderModel) {
+        this.store.update(
+            updateEntities(reminder.id, () => reminder),
+        )
     }
 
     public applyFilter(filters: ReminderFilterModel) {
