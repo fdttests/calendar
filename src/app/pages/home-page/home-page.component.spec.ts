3eb7fc17782ea0b4832382996736a054bf329fb9
Fdttests <fdttests@gmail.com>
2022-04-28 21:16:54 -0300

    refactor: add new imports to test

--- src/app/pages/home-page/home-page.component.spec.ts
+++ src/app/pages/home-page/home-page.component.spec.ts
@@ -1,5 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
+import { ColorPickerModule } from 'ngx-color-picker';
 import { CalendarModule } from 'src/app/components/calendar/calendar.module';
+import { ModalModule } from 'src/app/components/modal/modal.module';
 import { DateEventModalComponent } from './components/date-event-modal/date-event-modal.component';
 
 import { HomePageComponent } from './home-page.component';
@@ -10,13 +15,21 @@ describe('HomePageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomePageComponent ],
+      declarations: [
+        HomePageComponent,
+        DateEventModalComponent
+      ],
       imports: [
         CalendarModule,
-        DateEventModalComponent
+        ModalModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        ColorPickerModule,
+        OwlDateTimeModule,
+        OwlNativeDateTimeModule,
       ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
