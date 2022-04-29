6119e86d04f76c5f19fd671366882c81f52198c4
Fdttests <fdttests@gmail.com>
2022-04-28 21:16:50 -0300

    feat: use case to get first sunday of month

---
+++ src/app/data/use-cases/date/get-first-sunday-of-month.use-case.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed } from '@angular/core/testing';
+import { MonthEnum } from '../../enums/month.enum';
+
+import { GetFirstSundayOfMonthUseCase } from './get-first-sunday-of-month.use-case';
+
+describe('GetFirstSundayOfMonthUseCase', () => {
+  let service: GetFirstSundayOfMonthUseCase;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(GetFirstSundayOfMonthUseCase);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return the right sunday with given date', () => {
+    expect(service.execute(new Date(2022, MonthEnum.APRIL, 23))).toEqual(new Date(2022, MonthEnum.MARCH, 27));
+    expect(service.execute(new Date(2022, MonthEnum.MARCH, 15))).toEqual(new Date(2022, MonthEnum.FEBRUARY, 27));
+    expect(service.execute(new Date(2022, MonthEnum.FEBRUARY, 8))).toEqual(new Date(2022, MonthEnum.JANUARY, 30));
+    expect(service.execute(new Date(2022, MonthEnum.JANUARY, 10))).toEqual(new Date(2021, MonthEnum.DECEMBER, 26));
+  });
+});
---
+++ src/app/data/use-cases/date/get-first-sunday-of-month.use-case.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import UseCase from '../../base/use-case';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetFirstSundayOfMonthUseCase implements UseCase<Date, any> {
+  execute(date: Date): Date {
+    const [year, month] = [date.getFullYear(), date.getMonth()];
+    const firstDayOfMonth = new Date(year, month, 1);
+
+    const weekInterval = Array.from(Array(7).keys());
+
+    const firstSunday = weekInterval
+      .map(() => {
+        firstDayOfMonth.setDate(firstDayOfMonth.getDate() - 1);
+
+        return new Date(firstDayOfMonth);
+      })
+      .find((date: Date) => date.getDay() === 0);
+
+    if (!firstSunday) {
+      throw new Error('Unable to obtain first sunday of given date');
+    }
+
+    return firstSunday;
+  }
+}
